32dbd7619e030fad0735ba2fb6046250fadfda38
Nikhiljag <nikhilmukraj@gmail.com>
2022-07-20 21:12:42 -0400

    commands example

---
+++ commands_example.bat
@@ -0,0 +1,19 @@
+:: commands work like this
+:: command argument1 argument2 argument
+:: or
+:: command -flag argument1 -flag2 argument
+
+pip install numpy 
+:: use pip to install numpy
+
+python filename.py 
+:: run python file with filename
+
+git add .
+:: stage all changes to git
+
+git commit -m "commit thing"
+:: use git to commit the changes with the message "commit thing"
+
+git push
+:: use push changes to git repo using git
